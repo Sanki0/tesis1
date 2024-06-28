351b7eecf8d15d8c820448e395aa1d192cf232fb
Sanki0 <sebas_elias_999@hotmail.com>
2024-06-28 00:15:05 -0500

    Initial commit

---
+++ test/tesis1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tesis1 from '../lib/tesis1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tesis1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tesis1.Tesis1Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
